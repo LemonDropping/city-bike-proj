10c9775db8978db61465caab45efeeb6a7067473
gaughanln <gaughanln@yahoo.com>
2023-01-09 19:39:52 -0600

    fetch weather + bike api started

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,56 @@
 // API Variables
 var cityBikeApiUrl = "http://api.citybik.es/v2/networks";
-var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=a411ef0030322e0862cd44cde300dd84";
+var searchBtnEl = document.querySelector(".search-btn")
+var containerEl = document.querySelector(".container");
+var searchEl = document.querySelector(".search");
 
-// fetched CityBike API
+
+// setting display to none upon opening
+containerEl.style.display = "none";
+
+// event listener upon clicking search
+searchBtnEl.addEventListener("click", function (event) {
+  event.preventDefault();
+  containerEl.style.display = "block";
+
+  if (searchEl.value.trim() || searchEl.value.trim() !== "") {
+    let city = searchEl.value.trim();
+    // do the actual search
+    saveCitySearch(city);
+  }
+
+  // fetching the weather API data
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+      searchEl.value +
+      "&appid=a411ef0030322e0862cd44cde300dd84"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  console.log(searchEl.value);
+
+  // fetched CityBike API
 fetch(cityBikeApiUrl)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
-  // fetched Weather API
-fetch(weatherApiUrl)
 .then((response) => response.json())
 .then((data) => console.log(data));
+
+ var city = data.networks.location.city
+
+  // claring the search box
+  searchEl.value = "";
+});
+
+
+function saveCitySearch(city) {
+  let previousHistory = JSON.parse(localStorage.getItem("searchHistory")) || {};
+  previousHistory[city] = true;
+  localStorage.setItem("searchHistory", JSON.stringify(previousHistory));
+}
+
+
+
+
+
+
+
+
+
