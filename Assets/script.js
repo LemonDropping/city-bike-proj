678fe166691b9e23cf7e835ef110c3e8075b217a
gaughanln <gaughanln@yahoo.com>
2023-01-09 21:17:37 -0600

    the weather information is now showing on the page

--- Assets/script.js
+++ Assets/script.js
@@ -9,7 +9,7 @@ var searchEl = document.querySelector(".search");
 containerEl.style.display = "none";
 
 // event listener upon clicking search
-searchBtnEl.addEventListener("click", function (event) {
+searchBtnEl.addEventListener("click", function (event, data) {
   event.preventDefault();
   containerEl.style.display = "block";
 
@@ -22,18 +22,41 @@ searchBtnEl.addEventListener("click", function (event) {
   // fetching the weather API data
   fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
       searchEl.value +
-      "&appid=a411ef0030322e0862cd44cde300dd84"
+      "&appid=a411ef0030322e0862cd44cde300dd84&units=imperial"
   )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data)
+      document.querySelector(".temp").textContent = data.main.temp
+      console.log(data.main.temp)
+
+      document.querySelector(".description").textContent = data.weather[0].description
+      console.log(data.weather[0].description)
+
+      // can pull other icons from another source if you want
+      var weatherIcon = data.weather[0].icon
+      var weatherIconUrl = "https://openweathermap.org/img/wn/"+ weatherIcon +".png"
+      document.querySelector(".icon").setAttribute("src", weatherIconUrl) 
+      console.log(weatherIconUrl)
+
+
+    });
   console.log(searchEl.value);
+
+  
+  // things to call with weather api
+  // data.weather.0.icon
+  // data.sys.sunrise
+  // data.sys.sunset
+
 
   // fetched CityBike API
 fetch(cityBikeApiUrl)
 .then((response) => response.json())
 .then((data) => console.log(data));
 
- var city = data.networks.location.city
+// 
+//  var city = data.networks.location.city
 
   // claring the search box
   searchEl.value = "";
